e7fcc951c31b40ce6bea4f4354bda42207661997
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-08-23 18:15:35 -0300

    Aula 05 - Conhecendo Set

--- src/br/com/danilo/alura/teste/TestaAlunos.java
+++ src/br/com/danilo/alura/teste/TestaAlunos.java
@@ -1,16 +1,24 @@
 package br.com.danilo.alura.teste;
 
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
 public class TestaAlunos {
 
     public static void main(String[] args) {
-        Set<String> alunos = new HashSet<>();
+//        Set<String> alunos = new HashSet<>();
+        Collection<String> alunos = new HashSet<>(); // mais generico
         alunos.add("Rodrigo Turini");
         alunos.add("Alberto Souza");
         alunos.add("Nico Steppat");
+        alunos.add("Renan Saggio");
+        alunos.add("Mauricio Aniche");
+        alunos.add("Sergio Lopes");
+        alunos.add("Sergio Lopes"); // Somente um Sergio Lopes estará presente no conjunto
 
-        System.out.println(alunos);
+        alunos.forEach(aluno -> System.out.println(aluno));
+
+        boolean pauloEstaMatriculado = alunos.contains("Nico Steppat");
+        System.out.println(pauloEstaMatriculado);
     }
 }
